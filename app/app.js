6297423853a7a8d9a0a625e4b82620e31c7c663d
Ethan Schurkman <ethan.a.schurkman@gmail.com>
2019-08-28 16:53:34 -0400

    initial commit of scotch-player

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Search component
+class Search extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type = "text" />
+                <input type = "submit" />
+            </form>
+        );
+    }
+}
+
+ReactDOM.render( <Search />, document.getElementById('content'));
