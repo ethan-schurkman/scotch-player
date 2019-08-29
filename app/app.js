2a95b04f569f5fd700ee1968359a862b40f8d536
Ethan Schurkman <ethan.a.schurkman@gmail.com>
2019-08-29 10:53:34 -0400

    add in button, progress bar components

--- app/app.js
+++ app/app.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './components/search.component';
+import Details from './components/details.component';
+import Player from './components/player.component';
+import Progress from './components/progress.component';
 
 
 // component class
 class App extends React.Component {
     render() {
         return (
-            <Search />
+            <div>
+                <Search />
+                <Details title={'Track title'} />
+                <Player />
+                <Progress
+                  position={'0.75'}
+                  elapsed={'00:00'}
+                  total={'00:40'}
+                />
+            </div>
         );
     }
 }
